e3a699bfc62ca8fffd358c975f6d715046b417ee
James <james.re@yahoo.com>
2020-01-14 15:42:44 -0800

    2nd try to push

---
+++ isYes.js
@@ -0,0 +1,4 @@
+export default function isYes(str) {
+    if (str.toLowerCase() === 'yes' || str.toLowerCase() === 'y' || str.toLowerCase() === 'yea' || str.toLowerCase() === 'Y' || str.toLowerCase() === 'YES') return true;
+    else return false; 
+}
